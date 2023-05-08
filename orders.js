eff846a3c10c25191c5f7e99911fccfa0c9ddb93
Samuel Afolabi <samuelafo.212@gmail.com>
2023-05-08 07:29:42 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders =[
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '857-631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Another Sample Product',
+        productNumber: '357-565',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Beautiful Product Like that',
+        productNumber: '411-616',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Just an Amazing Product',
+        productNumber: '556-161',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'A Sample Product Name',
+        productNumber: '551-515',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
